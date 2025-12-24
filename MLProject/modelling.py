f948db84e92f02cef931fd941872ff2e5bf83cb5
depiana <devinanandaputri15@gmail.com>
2025-12-24 19:57:06 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,35 +5,36 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 import os
+import joblib  
 
 def train_model(learning_rate, n_estimators):
-    # Load dataset
     df = pd.read_csv("StudentsPerformance_preprocessing.csv")
 
-    # Target
     y = df["performance_level"]
-
-    # Encode target
     le = LabelEncoder()
     y = le.fit_transform(y)
 
-    # Features
     X = df.drop("performance_level", axis=1)
-
-    # One-hot encoding untuk fitur kategorikal
     X = pd.get_dummies(X)
 
     with mlflow.start_run():
-        model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+        model = RandomForestClassifier(
+            n_estimators=n_estimators,
+            random_state=42
+        )
         model.fit(X, y)
 
         mlflow.log_param("learning_rate", learning_rate)
         mlflow.log_param("n_estimators", n_estimators)
         mlflow.sklearn.log_model(model, "model")
 
-        # Simpan artefak
+    
+        joblib.dump(model, "model.pkl")
+
+        # Simpan artefak tambahan
         if not os.path.exists("artefak"):
             os.makedirs("artefak")
+
         X.describe().to_csv("artefak/data_summary.csv")
         mlflow.log_artifact("artefak/data_summary.csv")
 
